e5c7234563a71af26ffde75a4947e4cc629d9c41
Lian <laix678@gmail.com>
2024-06-10 13:01:34 +0800

    新增_修改密碼功能
    修正管理系統

--- manage.php
+++ manage.php
@@ -12,6 +12,9 @@ session_start();
     <title>帳號管理系統</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
+<style>
+</style>
+
 
 <body>
     <?php
@@ -46,9 +49,11 @@ session_start();
                         // echo "Login successful!";
                         // 託管不支援
                         // header('Location: view_records.php');
-                        echo "<script>alert('驗證成功！')</script>";
-                        echo "<script>
-                        window.location.href = 'manage.php';</script>";
+                        // echo "<script>alert('驗證成功！')</script>";
+                        include 'show_alert.php';
+                        show_toasts_success("驗證成功！");
+                        // echo "<script>
+                        // window.location.href = 'manage.php';</script>";
                         // exit();
                     } else {
                         echo "<script>alert('驗證失敗！')</script>";
@@ -66,6 +71,21 @@ session_start();
     <?php
     if (isset($_SESSION['v_user_id'])) {
         // 已驗證
+        // SQL 查詢語法
+        $query = "SELECT * FROM users WHERE id=" . $_SESSION['v_user_id'];
+        // 執行 SQL 查詢
+        $result = mysqli_query($link, $query);
+        // 取得第一筆資料
+        $row = mysqli_fetch_assoc($result); // 取得第一筆資料
+        // 顯示資料
+        echo "<h1>帳號管理系統</h1>";
+        echo "<h1>這裡應該要有一個form表單進行排版</h1>";
+        echo "<h2>歡迎 " . $row['username'] . " 回來！</h2>";
+        echo "<h3>您的使用者名稱是：" . $row['username'] . "</h3>";
+        echo "<h3>您的 email 是：" . $row['email'] . "</h3>";
+        echo "<h3>註冊時間：" . $row['created_at'] . "</h3>";
+
+
         echo "";
     } else {
         echo '<form method="POST" action="">
@@ -78,7 +98,7 @@ session_start();
                     </div>
                     <div class="modal-body">
                         <div class="form-floating mb-3">
-                            <input name="username" class="form-control" id="floatingInput">
+                            <input name="username" class="form-control" id="floatingInput" placeholder="Leave a comment here">
                             <label for="floatingInput">請輸入帳號：</label>
                         </div>
                         <div class="form-floating">
@@ -110,8 +130,65 @@ session_start();
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-    <button type="button" class="btn btn-warning" name="change_password">變更密碼</button>
-    <button type="button" class="btn btn-danger" name="deleter_user">刪除帳號</button>
+    <!-- 顯示按鈕 -->
+    <form action="" method="POST">
+        <button type="submit" class="btn btn-warning" name="change_password">變更密碼</button>
+        <button type="submit" class="btn btn-danger" name="deleter_user">刪除帳號</button>
+    </form>
+
+    <?php
+    // 變更密碼按鈕
+    if (isset($_POST['change_password'])) {
+        echo '<form action="" method="POST">
+                <div class="form-floating mb-3">
+                    <input name="old_password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">請輸入原本的密碼：</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input name="new_password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">請輸入要更改的密碼：</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input name="v_new_password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">請再次輸入要更改的密碼：</label>
+                </div>
+                <div class="col-auto">
+                    <button name="submit_new_password" class="btn btn-primary" type="submit">更改密碼</button>
+                </div>
+            </form>';
+    }
+    if (isset($_POST['deleter_user'])) {
+        echo "刪除帳號";
+    }
+
+    // 更改密碼
+    if (isset($_POST['submit_new_password'])) {
+        // 先確定有沒有輸入舊密碼和新密碼
+        if (isset($_POST['old_password']) && isset($_POST['new_password']) && isset($_POST['v_new_password'])) {
+            // 確認新密碼和再次輸入的新密碼是否相同
+            if ($_POST['new_password'] == $_POST['v_new_password']) {
+                // 確認舊密碼是否正確
+                $query = "SELECT * FROM users WHERE id=" . $_SESSION['v_user_id'];
+                $result = mysqli_query($link, $query);
+                $row = mysqli_fetch_assoc($result);
+                if (password_verify($_POST['old_password'], $row['password'])) {
+                    // 更新密碼
+                    $new_password = password_hash($_POST['new_password'], PASSWORD_DEFAULT);
+                    $query = "UPDATE users SET password='$new_password' WHERE id=" . $_SESSION['v_user_id'];
+                    if (mysqli_query($link, $query)) {
+                        echo "<script>alert('密碼更新成功！')</script>";
+                    } else {
+                        echo "<script>alert('密碼更新失敗！')</script>";
+                    }
+                } else {
+                    echo "<script>alert('舊密碼輸入錯誤！')</script>";
+                }
+            } else {
+                echo "<script>alert('新密碼和再次輸入的新密碼不相同！')</script>";
+            }
+        }
+    }
+    ?>
 </body>
 
 </html>
